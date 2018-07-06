e5c22c1294ae9de6a927ff5824a587aa98b2c094
Jorge Senín <jsenin@alea-soluciones.com>
2018-07-06 11:12:16 +0200

    Deleted yaml reader factory

--- infcommon/yaml_reader/factory.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import os
-
-from infcommon.factory.factory import Factory
-from infcommon.yaml_reader.yaml_reader import YamlReader
-
-def yaml_reader(file_conf=None):
-    return Factory.instance('config', lambda: YamlReader(file_conf or os.environ['CONF_FILE']))
