98d2f7ee58b98f13cf996dc8d630ba2f88a85f33
Jorge Senín <jsenin@alea-soluciones.com>
2018-07-06 11:25:39 +0200

    If file name not present, creates an empty yaml reader.

--- infcommon/settings_retriever/factory.py
+++ infcommon/settings_retriever/factory.py
@@ -7,11 +7,13 @@ from infcommon.settings_retriever.settings_retriever import SettingsRetriever
 from infcommon.yaml_reader.yaml_reader import YamlReader
 
 
-def _settings_file(file_name):
+def _settings_file(file_name=None):
+    if not file_name:
+        return {}
     return YamlReader(file_name)
 
 
-def settings_retriever(file_name):
+def settings_retriever(file_name=None):
     all_environment_values = dict(os.environ)
     settings_file = _settings_file(file_name)
     return Factory.instance('settings_retriever',
