ad25e748d3ed1acd7ec7e6e43a3002c5490f966c
Jorge Senín <jsenin@alea-soluciones.com>
2018-07-06 10:37:37 +0200

    Renaming config to settings

--- infcommon/settings_retriever/factory.py
+++ infcommon/settings_retriever/factory.py
@@ -5,12 +5,12 @@ from infcommon.settings_retriever.settings_retriever import SettingsRetriever
 from infcommon.yaml_reader.yaml_reader import YamlReader
 
 
-def _config_file(file_name):
+def _settings_file(file_name):
     return YamlReader(file_name)
 
 
-def settings_retriever(config_file_name):
+def settings_retriever(file_name):
     all_environment_values = dict(os.environ)
-    config_file = _config_file(config_file_name)
+    settings_file = _settings_file(file_name)
     return Factory.instance('settings_retriever',
-                            lambda: SettingsRetriever(all_environment_values, config_file))
+                            lambda: SettingsRetriever(all_environment_values, settings_file))
