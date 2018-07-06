0892d01c8e317ed1ddd189ee8d1112f2420b5516
Jorge Senín <jsenin@alea-soluciones.com>
2018-07-06 10:28:45 +0200

    Added settings retriever factory

---
+++ infcommon/settings_retriever/factory.py
@@ -0,0 +1,16 @@
+import os
+
+from infccomon.factory import Factory
+from infcommon.settings_retriever.settings_retriever import SettingsRetriever
+from infcommon.yaml_reader.yaml_reader import YamlReader
+
+
+def _config_file(file_name):
+    return YamlReader(file_name)
+
+
+def settings_retriever(config_file_name):
+    all_environment_values = dict(os.environ)
+    config_file = _config_file(config_file_name)
+    return Factory.instance('settings_retriever',
+                            lambda: SettingsRetriever(all_environment_values, config_file))
