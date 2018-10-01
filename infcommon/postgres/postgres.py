63b8afcba5a7ca9edeef291ca8663c1c7a7a1fe6
regiluze <regiluze@hotmail.com>
2018-10-01 18:09:22 +0200

    [Foolfix]

--- infcommon/postgres/postgres.py
+++ infcommon/postgres/postgres.py
@@ -7,7 +7,7 @@ from retrying import retry
 class PostgresClient(object):
 
     def __init__(self, user, password, host, port, db_name):
-        self._db_uri = 'postgresql://{user}:{password}@{host}:{port}/{db_name}'.format(user, password, host, port, db_name)
+        self._db_uri = 'postgresql://{user}:{password}@{host}:{port}/{db_name}'.format(user=user, password=password, host=host, port=port, db_name=db_name)
         self._connection = None
 
     def execute(self, query, args=None):
