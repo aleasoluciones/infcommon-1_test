c807823ef3aaaf048338c4880f7a53bd96ad8fc9
Daniel Baena <dbaena@alea-soluciones.com>
2018-10-02 10:41:30 +0200

    [INTERNAL] Make infcommon felix free

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(name='infcommon',
       version='0.0.1',
       author='Bifer Team',
-      description ='felix platform common infrastructure',
-      platforms = 'Linux',
+      description='Common Infrastructure Utilities',
+      platforms='Linux',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests'])
       )
 
